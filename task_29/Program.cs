5fba75dc6c6ac6b1a0ef95c42e4086f744d0b43a
ivan <senko.ivan999@mail.ru>
2022-09-04 11:14:57 +0300

    дз_4

---
+++ task_29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void RandomArray(int[] a){
+	Random random = new Random();
+	for(int i = 0; i < a.Length; i++){
+		a[i] = random.Next(0, 50);
+	
+	Console.Write(a[i] + " ");
+	}
+}
+
+int[] array = new int[8];
+RandomArray(array);
+
+// Подсмотрел в интернете немного)
